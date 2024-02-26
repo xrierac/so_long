b1348a20c7263d46bf449b4db388f74469da54e7
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-26 12:49:20 +0200

    Add map handling files

---
+++ src/load_map.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   load_map.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 10:33:02 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/02/26 11:57:44 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+/*
+int	load_map(t_mlx *mlx)
+{
+	int		fd;
+	char	*line;
+
+	line = "a";
+	fd = open(mlx->path, O_RDONLY);
+	if (fd < 0)
+		exit_error("Could not open the map\n");
+	while (line != NULL)
+	{
+		line = get_next_line(fd);
+		
+	}
+}
+*/
---
+++ src/map_error.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   map_error.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/26 09:53:38 by xriera-c          #+#    #+#             */
+/*   Updated: 2024/02/26 11:58:30 by xriera-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/so_long.h"
+/*
+static int	check_format(char *str)
+{
+	int		len;
+	char	*substr;
+
+	len = ft_strlen(str);
+	if (len < 5)
+		return (-1);
+	substr = &str[len - 4];
+	if (ft_strncmp(substr, ".ber", 4) != 0)
+		return (-1);
+	return (0);
+}
+
+static int	check_walls(char *str)
+{
+	if (str[0] != 1 || str[ft_strlen(str - 1)] != 1)
+		return (-1);
+}
+
+
+int	map_error(t_mlx *mlx)
+{
+	if (check_format(mlx->path) == -1)
+		exit_error("Wrong map format\n");
+		*/
