5d00e028cbcfce2fcb7500301f3c1576c26a9759
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-26 17:09:27 +0200

    Just add some tests for the new functions

--- src/so_long.c
+++ src/so_long.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 15:28:41 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/26 12:04:55 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/26 17:00:12 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,70 @@ void	exit_error(char *str)
 	exit(EXIT_FAILURE);
 }
 
+/*
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <memory.h>
+#include "MLX42/MLX42.h"
+#define WIDTH 5120
+#define HEIGHT 2880
+
+static void error(void)
+{
+	puts(mlx_strerror(mlx_errno));
+	exit(EXIT_FAILURE);
+}
+
+int32_t	main(void)
+{
+	// Start mlx
+	mlx_t* mlx = mlx_init(WIDTH, HEIGHT, "Test", true);
+	if (!mlx)
+        error();
+
+	// Create a new image
+	mlx_image_t* img = mlx_new_image(mlx, 512, 512);
+	if (!img)
+		error();
+
+	// Set every pixel to white
+	memset(img->pixels, 255, img->width * img->height * sizeof(int32_t));
+
+	// Display an instance of the image
+	if (mlx_image_to_window(mlx, img, 0, 0) < 0)
+        error();
+
+	mlx_loop(mlx);
+
+	// Optional, terminate will clean up any leftovers, this is just to demonstrate.
+	mlx_delete_image(mlx, img);
+	mlx_terminate(mlx);
+	return (EXIT_SUCCESS);
+}
+*/
 int	main(int argc, char *argv[])
 {
 	t_mlx	mlx;
+	int		i;
 
+	i = 0;
 	if (argc != 2)
 		return (write(2, "Wrong number of arguments\n", 26));
 	mlx.path = argv[1];
 	/*
-	load_map(argv[1]);
 	if (argv)
 	{
 		mlx = mlx_init(WIDTH, HEIGHT, "42Balls", true);
 		return (1);
-		*/
+	*/	
 	get_size(&mlx);
+	load_map(&mlx);
 	ft_printf("Width: %d Height: %d\n", mlx.width, mlx.height);
+	while (mlx.map[i])
+	{
+		ft_printf("%s\n", mlx.map[i]);
+		i++;
+	}
 	return (0);
 }
