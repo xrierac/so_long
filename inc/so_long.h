db4e7088d29c1bee15cbf3a394f75146c27a7ce4
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-02-26 12:14:05 +0200

    Add additional libraries and prototypes and definitions for get_next_line

--- inc/so_long.h
+++ inc/so_long.h
@@ -6,39 +6,49 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 13:58:25 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/25 13:00:39 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/02/26 12:05:48 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 # define SO_LONG_H
 
-# include <mlx.h>
+# include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+# include <fcntl.h>
+# include "MLX42/MLX42.h"
 # include "../lib/libft/libft.h"
 
-# define MLX_ERROR 1
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-typedef struct	s_win
+# ifndef FD_MAX
+#  define FD_MAX 256
+# endif
+
+typedef struct	s_mlx
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
+	void	*window;
+	void	*context;
 	int		width;
 	int		height;
-}	t_win;
-
-typedef struct	s_asset
-{
-	t_win	win;
-	void	*img_ptr;
-	char	*addr;
-	int		width;
-	int		height;
-	int		bpp;
-	int		endian;
-	int		size_line;
-}	t_asset;
+	double	delta_time;
+	char	*path;
+}	t_mlx;
 
 void	exit_error(char *str);
+int		map_error(char *str);
+int		load_map(char *str);
+void	get_size(t_mlx *mlx);
+
+//get_next_line//
+
+char	*get_next_line(int fd);
+char	*get_cache(char *cache, char *read_return, int fd);
+char	*get_next_line(int fd);
+char	*next_line(int fd, char *read_return, char *cache);
+char	*free_mem(char *res, char *tmp);
+
 #endif
