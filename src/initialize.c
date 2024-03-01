de62f315c2497939aecd2e4b08a62945e667a347
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-01 10:46:49 +0200

    Improve initialize_variables functiona and move away get_map_info

--- src/initialize.c
+++ src/initialize.c
@@ -6,7 +6,7 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/29 15:40:52 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/02/29 17:31:40 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/01 10:40:27 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,36 +14,11 @@
 
 void	initialize_variables(t_map *map)
 {
+	map->height = 0;
+	map->width = 0;
 	map->col = 0;
 	map->exit = 0;
 	map->player = 0;
-}
-
-void	get_map_info(t_map *map)
-{
-	int	i;
-	int	j;
-
-	i = -1;
-	while (map->map[++i])
-	{
-		j = -1;
-		while (map->map[i][++j])
-		{
-			if (map->map[i][j] == 'C')
-		   		map->col++;
-			if (map->map[i][j] == 'E')
-			{
-				map->exit++;
-				map->x_exit = j;
-				map->y_exit = i;
-			}
-			if (map->map[i][j] == 'P')
-			{
-				map->player++;
-				map->x_player = j;
-				map->y_player = i;
-			}
-		}
-	}
+	map->line = NULL;
+	map->map = NULL;
 }
