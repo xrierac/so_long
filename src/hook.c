c69fe35b6d07ff87913cbf868251dc4b8f690c2b
Xavier Riera Carrion <xriera-c@student.hive.fi>
2024-03-05 11:16:49 +0200

    Add move function and improve my_keyhook

--- src/hook.c
+++ src/hook.c
@@ -6,22 +6,37 @@
 /*   By: xriera-c <xriera-c@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:50:11 by xriera-c          #+#    #+#             */
-/*   Updated: 2024/03/04 16:46:30 by xriera-c         ###   ########.fr       */
+/*   Updated: 2024/03/05 11:11:23 by xriera-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/so_long.h"
 
-void my_keyhook(mlx_key_data_t keydata, t_map *param)
+//void	collect(t_map *map
+void	move(int x, int y, t_map *map)
 {
+	if (map->map[map->ply.y + y / SIZE][map->ply.x + x / SIZE] == '1')
+		return ;
+	map->img->ash->instances[0].x += x;
+	map->img->ash->instances[0].y += y;
+	map->ply.y = map->ply.y + y / SIZE;
+	map->ply.x = map->ply.x + x / SIZE;
+	ft_printf("%d %d\n", map->ply.x, map->ply.y);
+}
+
+void my_keyhook(mlx_key_data_t keydata, void *param)
+{
+	t_map	*map;
+
+	map = param;
 	if (keydata.key == MLX_KEY_ESCAPE && keydata.action == MLX_PRESS)
-		mlx_close_window(NULL);
+		mlx_close_window(map->mlx);
 	if (keydata.key == MLX_KEY_RIGHT && keydata.action == MLX_PRESS)
-		param->img->ash->instances[0].x += 1;
+		move(SIZE, 0, map);
 	if (keydata.key == MLX_KEY_LEFT && keydata.action == MLX_PRESS)
-		move(-SQ_SIZE, 0, param);
+		move(-SIZE, 0, map);
 	if (keydata.key == MLX_KEY_DOWN && keydata.action == MLX_PRESS)
-		move(0, SQ_SIZE, param);
+		move(0, SIZE, map);
 	if (keydata.key == MLX_KEY_UP && keydata.action == MLX_PRESS)
-		move(0, -SQ_SIZE, param);
+		move(0, -SIZE, map);
 }
